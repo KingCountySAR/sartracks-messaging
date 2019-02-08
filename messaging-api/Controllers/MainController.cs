a746f54ddb9ac20097078ad02ec33539614b9cde
Matt Cosand <matt@cosand.net>
2019-02-08 09:26:05 -0800

    Initial commit - accepts JWT from auth site

---
+++ messaging-api/Controllers/MainController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SarData.Common.Apis.Messaging;
+
+namespace SarData.Messaging.Api.Controllers
+{
+  [Authorize]
+  [ApiController]
+  public class MainController : ControllerBase
+  {
+
+    // GET api/values/5
+    [HttpPost("/send/email")]
+    public IActionResult SendEmail([FromBody] SendEmailRequest request)
+    {
+      return Ok(new { Data = new object { } });
+    }
+  }
+}
---
+++ messaging-api/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace SarData.Messaging.Api
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      BuildWebHost(args).Run();
+    }
+
+    public static IWebHost BuildWebHost(string[] args)
+    {
+      var builder = WebHost.CreateDefaultBuilder(args);
+      Console.WriteLine("Process ID: " + System.Diagnostics.Process.GetCurrentProcess().Id);
+      return builder
+        .UseStartup<Startup>()
+        .ConfigureAppConfiguration(config =>
+        {
+          config.AddJsonFile("appsettings.json", true, false)
+                .AddJsonFile("appsettings.local.json", true, false)
+                .AddEnvironmentVariables();
+        })
+        .Build();
+    }
+  }
+}
